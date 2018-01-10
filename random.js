73d09554953e0041d7c76b090bab08737d4a58b0
Flandre <flantan233@gmail.com>
2018-01-10 11:49:49 +0800

    update

--- random.js
+++ random.js
@@ -4,18 +4,25 @@ $(function () {
         timer;
 
     $("#start").click(function () {
-        var list = $("#list").val().replace(/ +/g, " ").replace(/^ | $/g, "").split(" ");
+        $('#what').show()
+        var list = [
+          1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+         11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+         21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+        ]
         if (!run) {
             heading.html("下一个中奖的会是谁？");
             $(this).val("停止");
             timer = setInterval(function () {
                 var r = Math.ceil(Math.random() * list.length),
-                    food = list[r - 1];
-                $("#what").html(food);
+                    num = list[r - 1];
+                $('.tens').attr('class', 'tens num-' + Math.floor(num / 10))
+                $('.unit').attr('class', 'unit num-' + Math.floor(num % 10))
+
                 var rTop = Math.ceil(Math.random() * $(document).height()),
                     rLeft = Math.ceil(Math.random() * ($(document).width() - 50)),
                     rSize = Math.ceil(Math.random() * (68 - 14) + 14);
-                $("<span class='temp'></span>").html(food).hide().css({
+                $("<span class='temp'></span>").html(num).hide().css({
                     "top": rTop,
                     "left": rLeft,
                     "color": "rgba(0,0,0,." + Math.random() + ")",
@@ -29,7 +36,7 @@ $(function () {
             run = 1;
         } else {
            heading.html("是这个！");
-            $(this).val("开始抽奖");
+            $(this).val("继续抽奖");
             clearInterval(timer);
             run = 0;
         };
